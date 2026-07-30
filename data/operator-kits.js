e5976f213e02dc60a3a653a64d0c89156629b5eb
lowlevelwizards <157194489+lowlevelwizards@users.noreply.github.com>
2026-07-29 22:55:22 -0400

    Data 0.4a

--- data/operator-kits.js
+++ data/operator-kits.js
@@ -1,4 +1,12 @@
 export const operatorKits = {
+  maintenance_worker_orange: {
+    id: "maintenance_worker_orange",
+    palette: { skin: "#d0b79b", hair: "#3d443d", headwear: "#544b39", helmetRear: "#403a2f", torso: "#6f7756", trousers: "#494c42", boots: "#282d29", belt: "#30362f", webbing: "#d28b42", rigPouch: "#84613e", backpack: "#4b5548", backpackFlap: "#5c6656", bedroll: "#75654c", weaponWood: "#73563d", weaponMetal: "#323b38", weaponButt: "#4c392b", hand: "#d0b79b", accent: "#e09a45" }
+  },
+  maintenance_worker_yellow: {
+    id: "maintenance_worker_yellow",
+    palette: { skin: "#c8aa8d", hair: "#363a35", headwear: "#394344", helmetRear: "#2c3435", torso: "#67725d", trousers: "#45483f", boots: "#242a26", belt: "#30362f", webbing: "#c7a449", rigPouch: "#796a45", backpack: "#445046", backpackFlap: "#566258", bedroll: "#71634d", weaponWood: "#73563d", weaponMetal: "#323b38", weaponButt: "#4c392b", hand: "#c8aa8d", accent: "#d5b45b" }
+  },
   field_operator_basic: {
     id: "field_operator_basic",
     palette: {
